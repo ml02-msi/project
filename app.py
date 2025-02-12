3797b264132518febd7efa7b4f6b3552e3647e29
ml02-msi <shaonballavstarinco@gmail.com>
2025-02-12 20:00:34 +0530

    Today_Last_commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
+import requests
 
 app = FastAPI()
 
@@ -24,32 +25,68 @@ tools = [
             "parameters": {
                 "type": "object",
                 "properties": {
-                    "url": {
+                    "script_url": {
                         "type": "string",
-                        "description": "URL of the script to run"
+                        "description": "The URL of the script to run"
                     },
                     "args": {
                         "type": "array",
                         "items": {
                             "type": "string"
                         },
-                        "description": "Arguments to pass to the script"
-                    }   
+                        "description": "List of arguments to pass to the script"
+                    },   
 
-                }
+                }, "required": ["script_url", "args"]
             }
-        }
+        },
     }
 ]
 
+AIPROXY_TOKEN = os.getenv("AIPROXY_TOKEN")
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/read")
 def read_file(path:str):
-    return {"path": path}
+    try:
+        with open(path, 'r') as f:
+            content = f.read()
+        return content
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    
+@app.get("/run")
+def task_runner(task:str):
+    url = "https://aiproxy.sanand.workers.dev/openai/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {AIPROXY_TOKEN}"
+    }
 
+    data = {   
+        "model": "gpt-4o-mini",
+        "messages": [
+            {
+                "role": "user",
+                "content": task
+            },
+            {
+                "role": "system",
+                "content": """
+You are an assistant who has to do a variety of tasks
+If your task involves running a script, you can use the script_runner tool.
+If your task involves writing a code, you can use the task_runner tool.
+"""
+            }
+        ],
+        "tools": tools,
+        "tools:choice": "auto"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
